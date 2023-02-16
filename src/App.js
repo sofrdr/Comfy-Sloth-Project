035ae78edce55783d38b2d357cd175600e90c6b9
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-02-16 13:25:43 +0100

    Configuration of the router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-
+import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  About,
+  Cart,
+  Products,
+  SingleProduct,
+  Checkout,
+  ErrorPage,
+} from "./pages";
 
 function App() {
   return (
-    <div>Comfy Sloth</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Checkout from "./CheckoutPage";
+import ErrorPage from "./ErrorPage";
+
+export { Home, About, Cart, Products, SingleProduct, Checkout, ErrorPage };
