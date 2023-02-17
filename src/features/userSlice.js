707b2ec6df1773a5adae290945dcbf9aea1c212d
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-02-17 12:53:07 +0100

    Store initialized with cartSlice and userSlice

---
+++ src/features/userSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
