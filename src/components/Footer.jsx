72b989da1745d9ee181839fb9ec127399f0541a6
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-02-17 13:23:26 +0100

    Footer styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  height: 90px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: var(--clr-black);
+  text-align: center;
+  p {
+    color: #fff;
+    margin: 0 5px;
+  }
+
+  @media screen and (min-width: 776px) {
+    flex-direction: row;
+  }
+
+  span {
+    color: var(--clr-primary-5);
+  }
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <p>
+        © 2023
+        <span>ComfySloth</span>
+      </p>
+      <p>All rights reserved</p>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
