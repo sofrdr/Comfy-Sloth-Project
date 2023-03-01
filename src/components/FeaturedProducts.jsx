dbb3bfc101e1f988d564634319c5d39b9d302c2b
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-03-01 11:14:59 +0100

    Hero component styled

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  background: var(--clr-grey-10);
+`;
 
 const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+  return (
+    <Wrapper className="section">
+      <div className="title">
+        <h2>Featured Products</h2>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default FeaturedProducts;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Hero, Services, FeaturedProducts, Contact } from "../components";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  );
 };
 
 export default HomePage;
